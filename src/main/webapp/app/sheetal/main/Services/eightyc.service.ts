9823bbc34cb889a7f8153087477452c7a9823f96
sheetal <sheetal.s@valuequo.com>
2018-08-13 12:47:36 +0530

    tax

--- src/main/webapp/app/sheetal/main/Services/eightyc.service.ts
+++ src/main/webapp/app/sheetal/main/Services/eightyc.service.ts
@@ -1,14 +1,24 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import { SERVER_API_URL } from 'app/app.constants';
-import { Eightyc } from 'app/sheetal/main/Services/eightyc.model';
-import { Subscription } from 'rxjs/Subscription';
-import { Component, OnInit } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+import { SERVER_API_URL } from "../../../app.constants";
+import { Eightyc } from "./eightyc.model";
+import { Subscription } from "rxjs/Subscription";
+import { Component, OnInit } from "@angular/core";
+import { AccountService } from "../../../shared";
 
 @Injectable()
 export class EightycService {
-  constructor(private http: HttpClient) {}
+  ID;
+  userID;
+  // temp: any = [];
+  user;
+  // id: any;
+  model: Eightyc = new Eightyc();
+
+  ServiceAPIParam: string;
+
+  constructor(private http: HttpClient, private account: AccountService) {}
 
   // public ServiceEightyc(eightyc) {
   // console.log(eightyc.Fixed);
@@ -25,6 +35,22 @@ export class EightycService {
   // console.log(eightyc.Tutionfee);
   // console.log(eightyc.Ulip);
   save(eightyc: any): Observable<any> {
-    return this.http.post(SERVER_API_URL + 'api/eightycs', eightyc);
+    return this.http.post(SERVER_API_URL + "api/eightycs", eightyc);
+  }
+  FetchID(): Promise<any> {
+    return this.account
+      .get()
+      .toPromise()
+      .then(response => {
+        this.user = response.body;
+        console.log("user info", this.user);
+        this.userID = this.user.id;
+        console.log("in service", this.userID);
+      });
+  }
+  public geteightyc(id) {
+    console.log("in geteightyc service", id);
+    this.ServiceAPIParam = "api/eightycs" + "/" + id;
+    return this.http.get(SERVER_API_URL + this.ServiceAPIParam).map(res => res);
   }
 }
