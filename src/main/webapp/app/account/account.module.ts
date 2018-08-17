0b5b74cf1dc8886a2dcd6f97a8b35862ffa6377e
sandeeppote <38154598+sandeeppote@users.noreply.github.com>
2018-08-17 11:05:55 +0530

    Merge branch 'master' of https://github.com/ValueQuoSPL/Front-Buckswise

--- src/main/webapp/app/account/account.module.ts
+++ src/main/webapp/app/account/account.module.ts
@@ -1,50 +1,53 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { BuckswiseFrontEndSharedModule, UserService } from 'app/shared';
-import { UserMgmtComponent } from 'app/admin';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { BuckswiseFrontEndSharedModule, UserService } from "app/shared";
+import { UserMgmtComponent } from "app/admin";
+
 import {
+  Register,
+  ActivateService,
+  PasswordService,
+  PasswordResetInitService,
+  PasswordResetFinishService,
+  PasswordStrengthBarComponent,
+  RegisterComponent,
+  ActivateComponent,
+  PasswordComponent,
+  PasswordResetInitComponent,
+  PasswordResetFinishComponent,
+  SettingsComponent,
+  accountState
+} from "app/account";
+import { CustomMaterialModule } from "app/custom-material.module";
+import { CustomDirectiveModule } from "app/shared/directive/directive.module";
+import { RecaptchaModule } from "ng-recaptcha";
+
+@NgModule({
+  imports: [
+    BuckswiseFrontEndSharedModule,
+    RouterModule.forChild(accountState),
+    CustomMaterialModule,
+    CustomDirectiveModule,
+    RecaptchaModule.forRoot()
+  ],
+  declarations: [
+    ActivateComponent,
+    RegisterComponent,
+    PasswordComponent,
+    PasswordStrengthBarComponent,
+    PasswordResetInitComponent,
+    PasswordResetFinishComponent,
+    SettingsComponent
+  ],
+  providers: [
     Register,
     ActivateService,
     PasswordService,
     PasswordResetInitService,
     PasswordResetFinishService,
-    PasswordStrengthBarComponent,
-    RegisterComponent,
-    ActivateComponent,
-    PasswordComponent,
-    PasswordResetInitComponent,
-    PasswordResetFinishComponent,
-    SettingsComponent,
-    accountState
-} from 'app/account';
-import { CustomMaterialModule } from 'app/custom-material.module';
-import { CustomDirectiveModule } from 'app/shared/directive/directive.module';
-
-@NgModule({
-    imports: [
-        BuckswiseFrontEndSharedModule,
-        RouterModule.forChild(accountState),
-        CustomMaterialModule,
-        CustomDirectiveModule
-    ],
-    declarations: [
-        ActivateComponent,
-        RegisterComponent,
-        PasswordComponent,
-        PasswordStrengthBarComponent,
-        PasswordResetInitComponent,
-        PasswordResetFinishComponent,
-        SettingsComponent
-    ],
-    providers: [
-        Register,
-        ActivateService,
-        PasswordService,
-        PasswordResetInitService,
-        PasswordResetFinishService,
-        UserMgmtComponent,
-        UserService
-    ],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    UserMgmtComponent,
+    UserService
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class BuckswiseFrontEndAccountModule {}
