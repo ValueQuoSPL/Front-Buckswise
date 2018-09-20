fdbeb4cd7ac763481a2b7dc90d3eaa17c07e3c8d
sandeeppote <38154598+sandeeppote@users.noreply.github.com>
2018-09-20 10:58:24 +0530

    chart_update

--- src/main/webapp/app/dashboard/dashboard.module.ts
+++ src/main/webapp/app/dashboard/dashboard.module.ts
@@ -1,11 +1,11 @@
-import { DashboardRoutingModule } from 'app/dashboard/dashboard.routes';
-import { NgModule } from '@angular/core';
-import { DashboardComponent } from 'app/dashboard/dashboard.component';
-import { DraggableModule } from 'app/shared/draggable/draggable.module';
-import { CustomMaterialModule } from 'app/custom-material.module';
-import { ChartsModule } from '../../../../../node_modules/ng2-charts';
-import { myasstsRoute } from 'app/my-assets/my-assets.route';
-import { RouterModule } from '../../../../../node_modules/@angular/router';
+import { DashboardRoutingModule } from "app/dashboard/dashboard.routes";
+import { NgModule } from "@angular/core";
+import { DashboardComponent } from "app/dashboard/dashboard.component";
+import { DraggableModule } from "app/shared/draggable/draggable.module";
+import { CustomMaterialModule } from "app/custom-material.module";
+import { ChartsModule } from "../../../../../node_modules/ng2-charts";
+import { myasstsRoute } from "app/my-assets/my-assets.route";
+import { RouterModule } from "../../../../../node_modules/@angular/router";
 
 @NgModule({
   imports: [
