3b13cf30e4127abf755b42d0d5d244dd1e0c7e3a
pratik-bali <37431032+pratik-bali@users.noreply.github.com>
2018-09-20 17:48:59 +0530

    subscription bug fix

--- src/main/webapp/app/home/subscriber/subscriber.component.ts
+++ src/main/webapp/app/home/subscriber/subscriber.component.ts
@@ -1,5 +1,5 @@
 import { Router, Route, ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
 import {PaymentComponent} from 'app/home/subscriber/payment/payment.component';
 import { Account, LoginModalService, Principal } from 'app/shared';
@@ -32,9 +32,10 @@ export class SubscriberComponent implements OnInit {
     plan;
 
     constructor(
-        private route: ActivatedRoute,
+        private zone: NgZone,
         private router: Router,
         private principal: Principal,
+        private route: ActivatedRoute,
         public activeModal: NgbActiveModal,
         private eventManager: JhiEventManager,
         private promoCodeService: PromoCodeService,
@@ -48,17 +49,20 @@ export class SubscriberComponent implements OnInit {
         });
         this.registerAuthenticationSuccess();
         this.promoCodeService.currentMessage.subscribe(
-            message => this.calculate(message)
+            message => this.zone.run(() => { // <== added
+                this.calculate(message);
+                console.log('change detection');
+            })
             );
         const plan = this.route.snapshot.params['plan'];
         this.plan = plan;
-        if (this.plan === 'free') {
+        if (this.plan === 'FREE') {
             this.payable = 0;
             this.oldAmount = this.payable;
-        } else if (this.plan === 'wiser') {
+        } else if (this.plan === 'WISER') {
             this.payable = 1000;
             this.oldAmount = this.payable;
-        } else if (this.plan === 'wisest') {
+        } else if (this.plan === 'WISEST') {
             this.payable = 2000;
             this.oldAmount = this.payable;
         }
@@ -86,6 +90,7 @@ export class SubscriberComponent implements OnInit {
             console.log('calculate not changed', this.applied);
 
         }
+
     }
 
     goToPayment() {
