305e1bd6e30542acc22ed24f730016d106b98ca3
pratik-bali <37431032+pratik-bali@users.noreply.github.com>
2018-10-04 17:19:31 +0530

    navbar transparent complete

--- src/main/webapp/app/shared/directive/number-only.directive.ts
+++ src/main/webapp/app/shared/directive/number-only.directive.ts
@@ -1,17 +1,17 @@
-import { Directive, ElementRef, HostListener } from "@angular/core";
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: "[jhiNumberOnly]"
+  selector: '[jhiNumberOnly]'
 })
 export class NumberOnlyDirective {
   // Allow decimal numbers and negative values
   private regex: RegExp = new RegExp(/^-?[0-9]+(\.[0-9]*){0,1}$/g);
   // Allow key codes for special events. Reflect :
   // Backspace, tab, end, home
-  private specialKeys: Array<string> = ["Backspace", "Tab", "End", "Home", "-"];
+  private specialKeys: Array<string> = ['Backspace', 'Tab', 'End', 'Home', '-'];
 
   constructor(private el: ElementRef) {}
-  @HostListener("keydown", ["$event"])
+  @HostListener('keydown', ['$event'])
   onKeyDown(event: KeyboardEvent) {
     //  console.log('number only directive');
     // Allow Backspace, tab, end, and home keys
