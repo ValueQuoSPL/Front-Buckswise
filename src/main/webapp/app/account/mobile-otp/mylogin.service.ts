44dbf730ba229a33142ec1d60785df789038c0cf
sandeeppote <38154598+sandeeppote@users.noreply.github.com>
2018-10-16 19:16:15 +0530

    otpkey_changes

--- src/main/webapp/app/account/mobile-otp/mylogin.service.ts
+++ src/main/webapp/app/account/mobile-otp/mylogin.service.ts
@@ -1,22 +1,35 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class MyloginService {
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient) { }
+  public submit(param) {
+    const template = "Hello%20";
+    const key = "243018AQaDBh0K5bc59c71";
+    const message = "Your%20Buckswise%20OTP%20is%20:%20";
+    const sender = "BKSWSE";
 
-    public submit(param) {
-        const template = 'Hello%20';
-        const key = '220095A0cp3pZGR5b1f7397';
-        const message = 'Your%20Buckswise%20OTP%20is%20:%20';
-        const sender = 'BKSWSE';
+    // tslint:disable-next-line:max-line-length
+    const url =
+      "http://control.msg91.com/api/sendotp.php?template=" +
+      template +
+      "&otp_length=6&authkey=" +
+      key +
+      "&message=" +
+      message +
+      param.otp +
+      "&sender=" +
+      sender +
+      "&mobile=91" +
+      param.mobile +
+      "&otp=" +
+      param.otp +
+      "&otp_expiry=60";
 
-        // tslint:disable-next-line:max-line-length
-        const url = 'http://control.msg91.com/api/sendotp.php?template=' + template + '&otp_length=6&authkey=' + key + '&message=' + message + param.otp + '&sender=' + sender + '&mobile=91' + param.mobile + '&otp=' + param.otp + '&otp_expiry=60';
-
-        // console.log('url :', url);
-        return this.http.get(url);
-     }
+    // console.log('url :', url);
+    return this.http.get(url);
+  }
 }
